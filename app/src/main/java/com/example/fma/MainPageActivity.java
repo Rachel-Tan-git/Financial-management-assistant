dd2e9543a668a97f31edbbc3ab2a84ed7cc662c4
tanchen <tanchen@deakin.edu.au>
2020-05-08 16:51:47 +0800

    fix error in the addRecordFragment.java and add some comments

--- app/src/main/java/com/example/fma/MainPageActivity.java
+++ app/src/main/java/com/example/fma/MainPageActivity.java
@@ -1,5 +1,6 @@
 package com.example.fma;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -7,28 +8,39 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainPageActivity extends AppCompatActivity {
 
     private String username;
+
+    private TextView welcomeText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        welcomeText = (TextView) findViewById(R.id.WelcomeText);
         Intent intent = getIntent();
         username = intent.getStringExtra("user_name");
-        /*Fragment frag;
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        //set the Username in the TextView to show the account
+        welcomeText.setText("Username: " + username);
+/*
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        fragments = new addRecordFragment();
 
-        frag = new addRecordFragment();
         Bundle bundle=new Bundle();
         bundle.putString("user_name",username);
-        frag.setArguments(bundle);
+        fragments.setArguments(bundle);
 
-        fragmentTransaction.replace(R.id.fragment, frag);
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();*/
+        transaction.show(fragments[0]);
+        transaction.replace(R.id.fragment, fragments[0]);
+
+        transaction.commit();
+
+ */
 
     }
 }
--- app/src/main/java/com/example/fma/addRecordFragment.java
+++ app/src/main/java/com/example/fma/addRecordFragment.java
@@ -13,6 +13,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.fma.Service.UserService;
@@ -20,19 +21,38 @@ import com.example.fma.Service.UserService;
 
 
 public class addRecordFragment extends Fragment {
-    /*private EditText recordType;
+    private EditText recordType;
     private EditText billName;
     private EditText billNumber;
     private EditText billDate;
     private RadioGroup billType;
     private Button addButton;
     private View view;
+    private String username;
 
+    //private TextView test;
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_add_record, container, false);
-        findView();
+        //find the views
+        recordType = (EditText)view.findViewById(R.id.type);
+        billName = (EditText)view.findViewById(R.id.billName);
+        billNumber = (EditText)view.findViewById(R.id.billNumber);
+        billDate = (EditText)view.findViewById(R.id.billDate);
+        billType = (RadioGroup)view.findViewById(R.id.BillType);
+        addButton = (Button)view.findViewById(R.id.addButton);
+
+        //test = (TextView)view.findViewById(R.id.test);
+        //get the username from the MainPageActivity
+        Bundle bundle=getArguments();
+        if(bundle != null) {
+           username = bundle.getString("user_name");
+
+        }
+        //test.setText(username);
+        //add a onClickListener for the addButton to add the bill record into the database
         addButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                //get the content form each EditText views and RadioGroup
                 String type = recordType.getText().toString().trim();
                 String name = billName.getText().toString().trim();
                 Double number = Double.parseDouble(billNumber.getText().toString().trim());
@@ -40,10 +60,9 @@ public class addRecordFragment extends Fragment {
                 String billTy = ((RadioButton)getActivity().findViewById(billType.getCheckedRadioButtonId())).getText().toString();
 
                 Log.i("TAG",billTy+"_"+type+"_"+name+"_"+number+"_"+date);
+                //use the UserService to connect the database
                 UserService userService=new UserService(getActivity());
-                Bundle bundle=getArguments();
-                String username=bundle.getString("one");
-
+                //use the userBill class to integrate data
                 userBill oneRecord = new userBill();
                 oneRecord.setUsername(username);
                 oneRecord.setBillType(billTy);
@@ -51,23 +70,14 @@ public class addRecordFragment extends Fragment {
                 oneRecord.setName(name);
                 oneRecord.setNumber(number);
                 oneRecord.setBillDate(date);
-
+                //add a record in to the userBill table in database
                 userService.addRecord(oneRecord);
                 Toast.makeText(getActivity(), "Add bill record successfully!", Toast.LENGTH_LONG).show();
             }
-
         });
         return view;
     }
 
-    private void findView(){
-        recordType = getActivity().findViewById(R.id.type);
-        billName = getActivity().findViewById(R.id.billName);
-        billNumber = getActivity().findViewById(R.id.billNumber);
-        billDate = getActivity().findViewById(R.id.billDate);
-        billType = getActivity().findViewById(R.id.BillType);
-        addButton = getActivity().findViewById(R.id.addButton);
-    }*/
 
 
 }
