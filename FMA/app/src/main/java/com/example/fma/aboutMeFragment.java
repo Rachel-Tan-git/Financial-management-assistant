222da120e23923f096a6ef05a2595c445f048c73
XINGHAO LI <lixingh@deakin.edu.au>
2020-05-18 23:29:33 +0800

    I developed the ability to generate PDF to store all bill records of users, but it needs to be further improved.

--- FMA/app/src/main/java/com/example/fma/aboutMeFragment.java
+++ FMA/app/src/main/java/com/example/fma/aboutMeFragment.java
@@ -1,32 +1,65 @@
 package com.example.fma;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.pdf.PdfDocument;
+//setContentRect
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.fma.Adapter.RecyclerAdapter;
+import com.example.fma.Service.UserService;
+import com.example.fma.userInforClass.userBill;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
 
 
 public class aboutMeFragment extends Fragment {
     private String username;
     private TextView usernameTextView;
     private TextView feedback;
-
+    private List<userBill> list;
+    private UserService userService;
+    private showRecordsViewModel viewModel;
+    private RecyclerView textContainer;
+    private RecyclerAdapter adapter;
+    private TextView print;
     private View view;
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         //find view
         view = inflater.inflate(R.layout.fragment_about_me, container, false);
         usernameTextView = (TextView)view.findViewById(R.id.username);
         feedback = (TextView)view.findViewById(R.id.feedback);
+        final PdfDocument document = new PdfDocument();
+        print = (TextView)view.findViewById(R.id.printRecords);
         //get the username from the MainPageActivity
         Bundle bundle=getArguments();
         if(bundle != null) {
@@ -59,6 +92,84 @@ public class aboutMeFragment extends Fragment {
                 }
             }
         });
+        print.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pdfInterviewContent(document);
+             if (android.os.Environment.getExternalStorageState().equals(
+                        android.os.Environment.MEDIA_MOUNTED)) {
+                 Toast.makeText(getActivity(), "1", Toast.LENGTH_SHORT).show();
+
+                } else  {
+                 Toast.makeText(getActivity(), "2", Toast.LENGTH_SHORT).show();   }
+            }
+        });
+
         return view ;
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        userService = new UserService(getContext());
+        viewModel = showRecordsViewModel.getINSTANCE(this);
+        //use the username to get all the userBill records from database
+        this.list = userService.showAllCharge(this.username);
+        //use the viewModel to processing the list data and share the data with adapter
+        viewModel.setLiveData(this.list);
+        //set the layout for the recyclerView
+        textContainer = requireActivity().findViewById(R.id.textContainer);
+        textContainer.setLayoutManager(new LinearLayoutManager(requireActivity()));
+        //set the adapter for the recyclerView
+        adapter = new RecyclerAdapter(viewModel);
+        textContainer.setAdapter(adapter);
+        //this step is equal to set the data to the adapter step by step
+        //if the database has more than 1 records then the adapter need a potion to find each record
+        //and then the adapter will set each of them to the recyclerView item
+        viewModel.getLiveData().observe(requireActivity(), new Observer<List<userBill>>() {
+            @Override
+            public void onChanged(List<userBill> records) {
+                adapter.setList(records);
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void pdfInterviewContent(PdfDocument document){
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            Toast.makeText(getActivity(), "get permission", Toast.LENGTH_SHORT).show();
+
+            ActivityCompat.requestPermissions(getActivity(), new String[]{
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
+        }
+        int onePageHeight = textContainer.getHeight()*30;
+        int lineCount = textContainer.getItemDecorationCount();
+        int pdfCount = lineCount % 30 == 0 ? lineCount/30 : lineCount/30+1;
+        for (int i = 0; i < pdfCount; i++) {
+            PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(textContainer.getWidth(),onePageHeight+120,1)
+                    .setContentRect(new Rect(0,60,textContainer.getWidth(),onePageHeight+60))
+                    .create();
+            PdfDocument.Page page = document.startPage(pageInfo);
+            Canvas canvas = page.getCanvas();
+            canvas.translate(0,-onePageHeight*i);
+            textContainer.draw(canvas);
+            document.finishPage(page);
+        }
+        try {
+            String path = "/mnt/sdcard/file.pdf";
+            File file = new File(path);
+            if (file.exists()) {
+                file.delete();
+            }
+        document.writeTo(new FileOutputStream(file));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        document.close();
+    }
+
 }
